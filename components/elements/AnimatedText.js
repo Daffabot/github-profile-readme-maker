e2d4b7a0e5f7b654f49f4f76b7b0661500230a8b
Daffabot <96946212+Daffabot@users.noreply.github.com>
2023-03-21 11:52:48 +0700

    Update AnimatedText.js

--- components/elements/AnimatedText.js
+++ components/elements/AnimatedText.js
@@ -4,19 +4,19 @@ import TextLoop from "react-text-loop/lib/components/TextLoop";
 export default function AnimatedText() {
   return (
     <div className="text-zinc-500 hidden md:block sm:text-2xl md:text-3xl lg:text-4xl 2xl:text-5xl font-medium overflow-y-hidden">
-      Create{" "}
+      Handra itu{" "}
       <TextLoop>
         <span className="text-rose-200 underline underline-offset-4 decoration-dashed">
-          Beautiful
+          Jelek
         </span>
         <span className="text-cyan-200 underline underline-offset-4 decoration-dashed">
-          Modern
+          Belegug
         </span>
         <span className="text-amber-200 underline underline-offset-4 decoration-dashed">
-          Interactive
+          Kang Copas
         </span>
         <span className="text-green-200 underline underline-offset-4 decoration-dashed">
-          Perfect
+          Asu
         </span>
       </TextLoop>{" "}
       Profile
